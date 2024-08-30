8a542f5875dccfbc817513d9f5395efbb623427b
0noor <noor.hassan@live.ca>
2024-08-30 13:06:29 -0400

    updates

--- PythonFlask/server.py
+++ PythonFlask/server.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request,redirect,url_for
+import pymongo
+from pymongo import MongoClient     
 app = Flask(__name__)
+
+client = MongoClient("localhost", 27017)
+
+db = client.todoDB
+
+todo = db.todo
+
+
+
+# todo.delete_many({"Task": "Finish Workout"})
+
+# for todo in todo.find():
+#     print(todo)
+
 
 @app.route('/')
 def hello_world():
-    return "<h1><b>Hello, Brother!</b></h1>"
+    return render_template("index.html")
 
-@app.route('/user')
-def greet_user():
-    return "<h1>Hello User</h1>"
+# @app.route('/user')
+# def greet_user():
+#     return "<h1>Hello User</h1>"
