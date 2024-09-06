621736f4f14f2fb156b6f7668d436b0e3664a8ee
0noor <noor.hassan@live.ca>
2024-09-06 12:59:11 -0400

    +3

--- PythonFlask/server.py
+++ PythonFlask/server.py
@@ -11,15 +11,23 @@ todo = db.todo
 
 
 
-# todo.delete_many({"Task": "Finish Workout"})
+# todo.delete_many({"Task": "Finish Workout", "done": False})
 
 # for todo in todo.find():
 #     print(todo)
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html",)
+
+@app.route('/addtodo', methods=["GET","POST"])
+def grab_task():
+    todo.insert_one({"Task": request.form['task'], 
+    "StartDate": request.form['startDate'],
+    "EndDate": request.form['endDate'],
+    "localTime": request.form['todaysDate']})
+    return redirect(url_for('index'))
 
 # @app.route('/user')
 # def greet_user():
