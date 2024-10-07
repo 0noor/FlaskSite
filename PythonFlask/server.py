e55d14ba735c8dee1b57b8d9587db3ddd299cda1
0noor <noor.hassan@live.ca>
2024-10-07 18:01:25 -0400

    +7

--- PythonFlask/server.py
+++ PythonFlask/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,redirect,url_for
 from pymongo import MongoClient     
 from bson.objectid import ObjectId
+import bcrypt
 app = Flask(__name__)
 
 client = MongoClient("localhost", 27017)
@@ -8,6 +9,8 @@ client = MongoClient("localhost", 27017)
 db = client.todoDB
 
 todos = db.todo
+
+users = db.user
 
 
 
@@ -22,7 +25,7 @@ def index():
     all_tasks = todos.find({}).sort({'dueDate': 1 })
     return render_template("index.html",todos = all_tasks)
 
-@app.route('/addtodo', methods=["GET","POST"])
+@app.route('/addtodo', methods=["POST"])
 def grab_task():
     todos.insert_one({"Task": request.form['task'], 
     "dueDate": request.form['dueDate'],
@@ -35,13 +38,26 @@ def delete(id):
     return redirect(url_for('index'))
 
 
-# @app.route('/add_todo', methods=['POST'])
-# def add_todo():
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":    
+        bytes =  request.form["password"].encode('utf-8')
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(bytes,salt)
+        users.insert_one({"username": request.form["username"],"email": request.form["email"],"salt": salt,"hash": hash})
+        return redirect(url_for("index"))
+    
+    return render_template("register.html")
+
+@app.route('/login', methods=["POST","GET"])
+def log_in():
+    if request.method == "POST":
+        return redirect(url_for("index"))
+    print(users.find({"email":  request.form["email"]}))
+    return render_template("login.html")
     
 
-# @app.route('/user')
-# def greet_user():
-#     return "<h1>Hello User</h1>"
 
 
 # todo.insert_one({"Task": "Finish Workout"})
